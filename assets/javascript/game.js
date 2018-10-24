0c911ac164eb1b7daee1fd805bce6f253431c29d
kjojones <43149867+kfroerer@users.noreply.github.com>
2018-10-23 21:32:24 -0500

    made some progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,35 +1,39 @@
 
-var words = [
-    ["L", "E", "S", "L", "I", "E"],
-    ["R", "O", "N"], 
-    ["A", "P", "R", "I", "L"],
-    ["A", "N", "D", "Y"],
-    ["T", "O", "M"],
-    ["J", "E", "R", "R", "Y"],
-    ["D", "O", "N", "N", "A"],
-    ["A", "N", "N"]
-];
+var wordBank =["Leslie", "Ron", "April", "Ann", "Donna", "Gerry"];
 var wins = 0
-var guessesLeft = 
-var wrongLetters = 0
- 
+var attempts = 10
+var wrongGuesses = []
 
-//picks name from array at random
-var random = Math.floor((Math.random()*(words.length-1))); 
 
-//word to guess will be chosen from array
-var guessWord = words[random];
+var compPick = function(){    
+   return wordBank[Math.floor((Math.random()*wordBank.length))];
+};
+console.log(compPick())
+var wordPick = compPick();
+console.log(wordPick.split(""))
+var splitWord = wordPick.split("")
 
-var letterSpaces = new Array(guessWord.length);
-
-//letter spaces resverved with an underscore
-for (var i = 0; i < letterSpaces.length; i++){
-	letters[i] = "_ ";
+for (var i = 0; i < splitWord.length; i++){
+    $("#computerWord").append("_ ");
 }
 
 
-function printWord () {
-    for (var i = 0; i < letterSpaces.length;)
-}
+
+
+$(document).on("keyup", function (event){
+    var userGuess = event.key;
+    console.log(userGuess);
+    
+    if (splitWord.indexOf(userGuess) !== -1){
+        console.log(splitWord.indexOf(userGuess))
+        //have letter show, make a function to loop through the array of the word to check if the index = userguess,
+    }else {
+    $("#wrongGuesses").append(userGuess);
+    }
+})
+
+// function printWord ()
+//     for (var i = 0; i < letterSpaces.length;)
+// }
 
 
